a04a30c0036a85fac388e014188bcfc934675fb0
Brooke Leinberger <bl2060@rit.edu>
2021-02-01 22:16:45 -0500

    term 2 changes

--- seashells.py
+++ seashells.py
@@ -2,3 +2,4 @@ print("She ", "ells ", "ea", "hell", sep="s" , end="")
 print("s down", sep="s" , end="")
 print(" by the", sep="s" , end=" ")
 print("sea", "shore", ".", sep="" , end="")
+Hellllloooooo
